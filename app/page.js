138f89735b4fa7210c8f9dc7dd17869fd0463437
andy19910102 <andy19910102@gmail.com>
2024-05-31 23:14:47 +0800

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,34 @@
-import Image from "next/image";
+// TODO: 取得 mock 資料夾內的對話清單假資料
+
+// TODO: 從 components 內載入 UserMessage 與 AIMessage 這兩個 React 元件
 
 export default function Home() {
+
+  // TODO: 設計一個函式處理表單送出時的邏輯，這個函式必須處理：
+  //       1. 能將使用者輸入的訊息使用POST Method送給後端API
+  //       2. 後端API回傳的AI回覆訊息必須加入到對話清單中
+
+  // TODO: 準備將取得到的對話清單假資料整理成可供渲染的格式(React元件)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="flex flex-col bg-gradient-to-r from-slate-100 to-slate-200 h-screen">
+        <div className="chat-room w-full h-full flex-1 overflow-y-scroll">
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <form
+          className="chat-input-group w-full flex px-3 py-3"
+        // TODO: 當表單送出時，設計一個函數負責處理相關邏輯
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <input
+            // TODO: 當輸入框的資料有變動時，需要有一個react的state(狀態)取得到輸入框的值
+            placeholder="Type your message here..."
+            type="text"
+            className="block flex-1 border-2 border-slate-200 rounded-l-md px-3 py-2 focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 transition duration-200"
+            required
+          />
+          <button type="submit" className="bg-indigo-500 text-white font-semibold rounded-r-md px-3 py-2 hover:opacity-90 focus:opacity-70 duration-200">Send</button>
+        </form>
+      </main>
+    </>
   );
 }
---
+++ mock/messageListData.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        id: "1",
+        text: "你好，你是誰？",
+        createdBy: "user",
+        createdAt: 1717167000000
+    },
+    {
+        id: "2",
+        text: "您好，我是您的客服聊天機器人，有什麼可以幫助您的嗎？",
+        createdBy: "ai",
+        createdAt: 1717167010000
+    },
+    {
+        id: "3",
+        text: "我忘記了我的密碼，怎麼辦？",
+        createdBy: "user",
+        createdAt: 1717167020000
+    },
+    {
+        id: "4",
+        text: "您可以點擊忘記密碼的連結，然後按照指示重置密碼。",
+        createdBy: "ai",
+        createdAt: 1717167030000
+    },
+    {
+        id: "5",
+        text: "重置密碼需要多久時間？",
+        createdBy: "user",
+        createdAt: 1717167040000
+    },
+    {
+        id: "6",
+        text: "通常只需要幾分鐘，您會收到一封包含重置連結的電子郵件。",
+        createdBy: "ai",
+        createdAt: 1717167050000
+    },
+    {
+        id: "7",
+        text: "如果沒有收到重置郵件怎麼辦？",
+        createdBy: "user",
+        createdAt: 1717167060000
+    },
+    {
+        id: "8",
+        text: "請檢查您的垃圾郵件夾，如果還是找不到，請聯繫我們的客服支援。",
+        createdBy: "ai",
+        createdAt: 1717167070000
+    },
+    {
+        id: "9",
+        text: "客服支援的聯繫方式是什麼？",
+        createdBy: "user",
+        createdAt: 1717167080000
+    },
+    {
+        id: "10",
+        text: "您可以通過電話或電子郵件聯繫我們，詳情請參閱我們的官網。",
+        createdBy: "ai",
+        createdAt: 1717167090000
+    },
+    {
+        id: "11",
+        text: "好的，謝謝你。",
+        createdBy: "user",
+        createdAt: 1717167100000
+    },
+    {
+        id: "12",
+        text: "不客氣，祝您有美好的一天！",
+        createdBy: "ai",
+        createdAt: 1717167110000
+    }
+];
